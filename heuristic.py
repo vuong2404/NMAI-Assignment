270c8b5c3f08cf3396653007752deab7bff9ccf2
truongtran19315 <truong.chiptttran@gmail.com>
2023-10-28 18:22:16 +0700

    edit and update hill climbing

--- heuristic.py
+++ heuristic.py
@@ -11,7 +11,8 @@ class NQueens:
 
     def initialize(self):
         # randomly place one queen in each column
-        self.board = [random.randint(0, self.n - 1) for _ in range(self.n)]
+        self.board = list(range(self.n)) # create a list of numbers from 0 to n-1
+        random.shuffle(self.board) # shuffle the list to get a random permutation
         # calculate the initial conflicts for each queen
         self.conflicts = {i: self.calculate_conflicts(i) for i in range(self.n)}
         # calculate the initial total conflicts
@@ -20,25 +21,30 @@ class NQueens:
     def calculate_conflicts(self, i, new_row=None):
         # calculate the number of conflicts for the queen at column i
         # conflicts occur when two queens are in the same row, diagonal, or anti-diagonal
-        row = self.board[i]
-        conflicts = 0
+        row = new_row if new_row is not None else self.board[i]
+        rows = set() # a set of rows that have queens
+        diags = set() # a set of diagonals that have queens
+        anti_diags = set() # a set of anti-diagonals that have queens
         for j in range(self.n):
             if j == i: continue # skip the same column
-            if self.board[j] == row: conflicts += 1 # same row
-            if abs(self.board[j] - row) == abs(j - i): conflicts += 1 # same diagonal or anti-diagonal
+            rows.add(self.board[j]) # add the row of the queen at column j to the set
+            diags.add(self.board[j] - j) # add the diagonal of the queen at column j to the set
+            anti_diags.add(self.board[j] + j) # add the anti-diagonal of the queen at column j to the set
+        conflicts = 0
+        if row in rows: conflicts += 1 # same row
+        if row - i in diags: conflicts += 1 # same diagonal
+        if row + i in anti_diags: conflicts += 1 # same anti-diagonal
         return conflicts
 
     def update_conflicts(self, i, new_row):
         # update the conflicts for the queen at column i and its neighbors after moving to a new row
         old_row = self.board[i]
         self.board[i] = new_row
+        self.total_conflicts = 0 # reset the total conflicts
         for j in range(self.n):
-            if j == i: continue # skip the same column
-            old_conflicts = self.conflicts[j]
-            new_conflicts = self.calculate_conflicts(j)
-            self.conflicts[j] = new_conflicts
-            self.total_conflicts += (new_conflicts - old_conflicts)
-        self.conflicts[i] = 0
+            self.conflicts[j] = self.calculate_conflicts(j) # recalculate the conflicts for each queen
+            self.total_conflicts += self.conflicts[j] # update the total conflicts
+        self.total_conflicts //= 2
 
     def solve(self):
         # try to find a solution using hill climbing algorithm
@@ -82,8 +88,8 @@ class NQueens:
                     line += ". "
             print(line)
 
-# test the program with n = 100000
-nq = NQueens(10)
+# test the program with n = 10
+nq = NQueens(20)
 nq.initialize()
 nq.solve()
 nq.print_board() # uncomment this line to print the board (not recommended for large n)
